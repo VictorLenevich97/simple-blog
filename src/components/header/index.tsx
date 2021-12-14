e2c20d679aa5dbbf6911ce90f65b0e60e592d7ce
VictorLenevich97 <victorlenevich@yandex.by>
2021-12-14 14:30:05 +0300

    [ADD]: Added routes enum

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,13 +2,14 @@ import { Link } from 'gatsby';
 import { useThemeContext } from 'hooks/useThemeContext';
 import { THEME_TYPES } from 'types/themeTypes';
 import { usePathName } from 'hooks/usePathName';
+import { ROUTES } from 'constant/routes';
 
 import * as S from './header.styled';
 
 const NAVIGATION_DATA = [
-    { id: 1, title: 'Blog', link: '/' },
-    { id: 2, title: 'About me', link: '/about' },
-    { id: 3, title: 'Contacts', link: '/contacts' },
+    { id: 1, title: 'Blog', link: ROUTES.HOME_PAGE },
+    { id: 2, title: 'About me', link: ROUTES.ABOUT_ME },
+    { id: 3, title: 'Contacts', link: ROUTES.CONTACTS },
 ];
 
 export const Header = () => {
---
+++ src/constant/routes.ts
@@ -0,0 +1,5 @@
+export enum ROUTES {
+    HOME_PAGE = '/',
+    ABOUT_ME = '/about',
+    CONTACTS = '/contacts',
+}
